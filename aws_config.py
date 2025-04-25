344a2518f61c8c4adc5fb1a238167d504cba0ce9
Chinmay Agarwal <76059604+chinmayagarwal3007@users.noreply.github.com>
2025-04-25 16:04:29 +0530

    Update aws_config.py

--- aws_config.py
+++ aws_config.py
@@ -3,5 +3,5 @@ import boto3
 def get_bedrock_runtime_client():
     return boto3.client(
         service_name="bedrock-runtime",
-        region_name="us_east_1"
+        region_name="us-east-1"
     )
